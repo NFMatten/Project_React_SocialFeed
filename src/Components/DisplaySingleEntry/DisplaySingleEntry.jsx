45450f7b96862eae0c114438181d0e328182d835
Nikolaus Matten <nfmatten@gmail.com>
2022-10-19 11:07:15 -0600

    Updated styling for DisplaySingleEntry component

--- src/Components/DisplaySingleEntry/DisplaySingleEntry.jsx
+++ src/Components/DisplaySingleEntry/DisplaySingleEntry.jsx
@@ -2,7 +2,7 @@ import "./DisplaySingleEntry.css"
 
 const DisplaySingleEntry = (props) => {
     return ( 
-        <div>
+        <div className="entry-border">
             <p className="person-styling">{props.entry.person}</p>
             <p className="message-styling">{props.entry.post}</p>
         </div>
